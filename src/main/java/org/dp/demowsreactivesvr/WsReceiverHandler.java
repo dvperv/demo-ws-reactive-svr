9f4a02a97bb699834310c56df5015937fa5d71f4
dvperv <dvperv@gmail.com>
2023-03-20 18:58:12 +0300

    Initial

--- src/main/java/org/dp/demowsreactivesvr/WsReceiverHandler.java
+++ src/main/java/org/dp/demowsreactivesvr/WsReceiverHandler.java
@@ -8,12 +8,7 @@ public class WsReceiverHandler implements WebSocketHandler {
     @Override
     public Mono<Void> handle(WebSocketSession session) {
         return session.receive()
-                .doOnNext(message -> {
-                    System.out.println(message.getPayloadAsText());
-                })
-//                .concatMap(message -> {
-//                    // ...
-//                })
+                .doOnNext(message -> System.out.println(message.getPayloadAsText()))
                 .then();
     }
 }
